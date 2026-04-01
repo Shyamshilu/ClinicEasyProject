f501dab34a128eb394a22faa5e55cb1665ee6277
Shyamshilu <your-email@example.com>
2026-04-01 16:17:01 +0530

    fix media images

--- ClinicEasy/settings.py
+++ ClinicEasy/settings.py
@@ -132,9 +132,9 @@ USE_TZ = True
 
 
 # Media files (uploads)
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
