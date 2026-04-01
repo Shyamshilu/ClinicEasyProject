a59d57709332eb92961b800de805ef1f70b74b75
Shyamshilu <shyamshilu77@gmail.com>
2026-04-01 11:29:47 +0530

    as

--- doctors/urls.py
+++ doctors/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from doctors import views
 
 urlpatterns = [
     path("doctor/dashboard/", views.doctor_dashboard, name="doctor_dashboard"),
     path('doctors/', views.doctor_list, name='doctors'),
+    path('add-doctors/', views.add_doctors),
     path('doctor/<int:doctor_id>/', views.doctor_detail, name='doctor_detail'),
 ]
